7602dccd330672c206f3238917f5f1882045f521
JordynK91 <JordynK91@gmail.com>
2018-01-19 14:31:14 -0500

    clock is operational

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-
- function time() {
+function time() {
 	var d = new Date();
 	var h = d.getHours();
 	var m = d.getMinutes();
-	var s = d.getMinutes();
+	var s = d.getSeconds();
 	var clock = document.getElementById('clock');
-	clock.innerHTML = h.toString() + (":") + m.toString() + (":") + s.toString();
-}
+	if (s < 10){
+		var sec = ("0" + s.toString())
+		} 
+	else {
+		var sec = (s.toString())
+		}
+	if (m < 10){
+		var min = ("0" + m.toString())
+		} 
+	else {
+		var min = (m.toString())
+		}
+	if (h < 10){
+		var hr = ("0" + h.toString())
+		} 
+	else {
+		var min = (h.toString())
+		}
+	clock.innerHTML = h.toString() + (":") + m.toString() + (":") + sec
+	}
 
 
-setInterval(function time),1000)
+var t = setInterval(function(){time()}, 1000)
 
